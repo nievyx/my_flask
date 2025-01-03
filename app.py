59f570f7de596dbe89d2adedaa41f59374f64414
nievyx <84552647+nievyx@users.noreply.github.com>
2025-01-03 02:12:16 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 Scss(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config['SQLAlCHEMY_TRACK_MODIFICATION'] = False
 db = SQLAlchemy(app)
 
 #Data Class ~ Row of Data
@@ -43,10 +44,36 @@ def index():
 
 
 
+# Delete an Item
+@app.route('/delete/<int:id>')
+def delete(id:int):
+    delete_task = MyTask.query.get_or_404(id)
+    try:
+        db.session.delete(delete_task)
+        db.session.commit()
+        return redirect('/')
+    except Exception as e:
+        return f'ERROR:{e}'
 
+
+
+#Edit an item
+@app.route('/edit/<int:id>', methods=['GET','POST'])
+def edit(id:int):
+    task= MyTask.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            return f'ERROR:{e}'
+    else:
+        return render_template('edit.html', task=task)
+    
+with app.app_context():
+        db.create_all()
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
 
     app.run(debug=True)
